9e2cdf83a792c98a8317e6e37468b29c3b8fa2c1
sosoxuc <ioseb.khutsishvili@gmail.com>
2022-02-19 18:57:01 +0400

    Vesting test

--- test/test-distribute-to-dls-dao.js
+++ test/test-distribute-to-dls-dao.js
@@ -81,5 +81,6 @@ describe("LandDAO Distribute to DLS DAO", function () {
     await landDAO.distributeToDlsDao(maxSupply);
     const balance = await landDAO.balanceOf(owner.address);
     expect(balance).to.equal(maxSupply);
+    await landDAO.distributeToDlsDao(1);
   });
 });
