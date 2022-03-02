8656dc332f978481b51ae5c05b461aef98b00328
Erekle98 <e.lomsadze1@gmail.com>
2022-03-02 11:10:46 +0400

    Add contracts verifications

--- scripts/verify.js
+++ scripts/verify.js
@@ -19,11 +19,77 @@ async function main() {
     constructorArguments: [
       "TestDAO",
       "TEST",
-      "0x3f33eea734b01ec9e9bd1b44a3eb80c36ba585be"
+      "0x3f33eea734b01ec9e9bd1b44a3eb80c36ba585be",
     ],
   });
 
   console.log("Source Verified");
+
+  await hre.run("verify:verify", {
+    address: "0xD8598Ce75aF7BeA915A59B1B0b13c1543dd59EF3",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("dlsDao verified");
+
+  await hre.run("verify:verify", {
+    address: "0x7fe1b383dFef2dFf99c89183E122FD969D79f3fF",
+    constructorArguments: ["0x18Bb2c53E9f8C38E5BddF60955f462833705bD80"],
+  });
+
+  console.log("dlsNftOwner verified");
+
+  await hre.run("verify:verify", {
+    address: "0xec274E81762D70C21DE9081c9DF947D49589889B",
+    constructorArguments: [
+      "0x938323b4fcBcD564332E7B3392dd07b7Ec755E49",
+      "0x825D66E34A1df88E1a977C69404e5dE8b3e7D8Ea",
+    ],
+  });
+
+  console.log("landOwner verified");
+
+  await hre.run("verify:verify", {
+    address: "0xaD43c590290a58723B8F0b87D1E4cb7b746aFD32",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("liquidity verified");
+
+  await hre.run("verify:verify", {
+    address: "0x455e12E4B256CF90e486b05a669E9d469EF45599",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("pullRewards verified");
+
+  await hre.run("verify:verify", {
+    address: "0x9606866618e6812b6ff1c9b3844e2878d9ad0651",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("staking verified");
+
+  await hre.run("verify:verify", {
+    address: "0x5bd6eede29c7605ae7b7d0ca7a5eec4a384f8869",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("strategicSales verified");
+
+  await hre.run("verify:verify", {
+    address: "0xec97d8efa24f6801d422c87ed7d4088ad1a0b6a9",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("team verified");
+
+  await hre.run("verify:verify", {
+    address: "0x3bf1ef011f6e7f058d05d7f45b0ae23983c46bc4",
+    constructorArguments: ["0x938323b4fcBcD564332E7B3392dd07b7Ec755E49"],
+  });
+
+  console.log("treasury verified");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
