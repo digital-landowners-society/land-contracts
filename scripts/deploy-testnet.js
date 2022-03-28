9dd2d57fbef0eb852a5af77824675c730714007d
sosoxuc <ioseb.khutsishvili@gmail.com>
2022-03-28 10:40:29 +0400

    Testnet config

---
+++ scripts/deploy-testnet.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+
+async function main() {
+  const LandDao = await hre.ethers.getContractFactory("LandDAO");
+  const landDao = await LandDao.deploy(
+    "TestDAO",
+    "TEST",
+    "0xea8b08fdf5865b16cea49cbfc20bc8bf64da6e97"
+  );
+  await landDao.deployed();
+  console.log("LandDao deployed to:", landDao.address);
+
+  await hre.run("verify:verify", {
+    address: landDao.address,
+    constructorArguments: [
+      "TestDAO",
+      "TEST",
+      "0xea8b08fdf5865b16cea49cbfc20bc8bf64da6e97",
+    ],
+  });
+  console.log("Source Verified");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
