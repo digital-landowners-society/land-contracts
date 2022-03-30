db96b8e97ff5d4ec5973c77c98229828749bf94b
sosoxuc <ioseb.khutsishvili@gmail.com>
2022-03-30 15:45:57 +0400

    Improve testnet deployment code

--- scripts/deploy-lp-staking-testnet.js
+++ scripts/deploy-lp-staking-testnet.js
@@ -3,10 +3,8 @@ const hre = require("hardhat");
 async function main() {
   const LandLpStaking = await hre.ethers.getContractFactory("LPStaking");
   const landLpStaking = await LandLpStaking.deploy(
-    "xTestDAO",
-    "xTEST",
     "0x9792C97C6EC5361289BE9392E601a58222b18f66", // LandDAO
-    "0x77cf6f9b71bb02857e8b341cee3130b4f7f996e0", // LP
+    "0x783651a701678f1C18416D5DbA6cF34aC5fEa242", // LP
   );
   await landLpStaking.deployed();
   console.log("LandDao staking deployed to:", landLpStaking.address);
@@ -14,10 +12,8 @@ async function main() {
   await hre.run("verify:verify", {
     address: landLpStaking.address,
     constructorArguments: [
-      "xTestDAO",
-      "xTEST",
       "0x9792C97C6EC5361289BE9392E601a58222b18f66", // LandDAO
-      "0x77cf6f9b71bb02857e8b341cee3130b4f7f996e0", // LP
+      "0x783651a701678f1C18416D5DbA6cF34aC5fEa242", // LP
     ],
   });
   console.log("Source Verified");
